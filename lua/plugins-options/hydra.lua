60243857dcb02aed78d7119c88adc97e25927cb1
hady.elzayady <hady.elzayady@pixelogicmedia.com>
2023-08-06 16:21:11 +0300

    merge conflicts

--- lua/plugins-options/hydra.lua
+++ lua/plugins-options/hydra.lua
@@ -1,8 +1,8 @@
-local M={}
+local M = {}
 function M.gitsigns_menu()
-  local gitsigns = require "gitsigns"
+	local gitsigns = require("gitsigns")
 
-  local hint = [[
+	local hint = [[
  _J_: Next hunk   _s_: Stage Hunk        _d_: Show Deleted   _b_: Blame Line
  _K_: Prev hunk   _u_: Undo Last Stage   _p_: Preview Hunk   _B_: Blame Show Full
  ^ ^              _S_: Stage Buffer      ^ ^                 _/_: Show Base File
@@ -10,166 +10,122 @@ function M.gitsigns_menu()
  ^ ^              _<Enter>_: Neogit              _q_: Exit
 ]]
 
-  return {
-    name = "Git",
-    hint = hint,
-    config = {
-      color = "pink",
-      invoke_on_body = true,
-      hint = {
-        border = "rounded",
-        position = "bottom",
-      },
-      on_enter = function()
-        vim.cmd "mkview"
-        vim.cmd "silent! %foldopen!"
-        vim.bo.modifiable = false
-        gitsigns.toggle_signs(true)
-        gitsigns.toggle_linehl(true)
-      end,
-      on_exit = function()
-        vim.cmd("silent! DiffviewClose")
-        gitsigns.toggle_signs(false)
-        gitsigns.toggle_linehl(false)
-        gitsigns.toggle_deleted(false)
-      end,
-    },
-    body = "<space>g.",
-    heads = {
-      {
-        "J",
-        function()
-          if vim.wo.diff then
-            return "]c"
-          end
-          vim.schedule(function()
-            gitsigns.next_hunk()
-          end)
-          return "<Ignore>"
-        end,
-        { expr = true, desc = "Next Hunk" },
-      },
-      {
-        "K",
-        function()
-          if vim.wo.diff then
-            return "[c"
-          end
-          vim.schedule(function()
-            gitsigns.prev_hunk()
-          end)
-          return "<Ignore>"
-        end,
-        { expr = true, desc = "Prev Hunk" },
-      },
-      { "s", ":Gitsigns stage_hunk<CR>", { silent = true, desc = "Stage Hunk" } },
-      { "u", gitsigns.undo_stage_hunk,   { desc = "Undo Last Stage" } },
-      { "S", gitsigns.stage_buffer,      { desc = "Stage Buffer" } },
-      { "p", gitsigns.preview_hunk,      { desc = "Preview Hunk" } },
-      { "d", gitsigns.toggle_deleted,    { nowait = true, desc = "Toggle Deleted" } },
-      { "b", gitsigns.blame_line,        { desc = "Blame" } },
-      {
-        "B",
-        function()
-          gitsigns.blame_line { full = true }
-        end,
-        { desc = "Blame Show Full" },
-      },
-      { "/",       gitsigns.show,     { exit = true, desc = "Show Base File" } }, -- show the base of the file
-      { "<Enter>", "<Cmd>Neogit<CR>", { exit = true, desc = "Neogit" } },
-      { "q",       nil,               { exit = true, nowait = true, desc = "Exit" } },
-    },
-  }
+	return {
+		name = "Git",
+		hint = hint,
+		config = {
+			color = "pink",
+			invoke_on_body = true,
+			hint = {
+				border = "rounded",
+				position = "bottom",
+			},
+			on_enter = function()
+				vim.cmd("mkview")
+				vim.cmd("silent! %foldopen!")
+				vim.bo.modifiable = false
+				gitsigns.toggle_signs(true)
+				gitsigns.toggle_linehl(true)
+			end,
+			on_exit = function()
+				vim.cmd("silent! DiffviewClose")
+				gitsigns.toggle_signs(false)
+				gitsigns.toggle_linehl(false)
+				gitsigns.toggle_deleted(false)
+			end,
+		},
+		body = "<space>g.",
+		heads = {
+			{
+				"J",
+				function()
+					if vim.wo.diff then
+						return "]c"
+					end
+					vim.schedule(function()
+						gitsigns.next_hunk()
+					end)
+					return "<Ignore>"
+				end,
+				{ expr = true, desc = "Next Hunk" },
+			},
+			{
+				"K",
+				function()
+					if vim.wo.diff then
+						return "[c"
+					end
+					vim.schedule(function()
+						gitsigns.prev_hunk()
+					end)
+					return "<Ignore>"
+				end,
+				{ expr = true, desc = "Prev Hunk" },
+			},
+			{ "s", ":Gitsigns stage_hunk<CR>", { silent = true, desc = "Stage Hunk" } },
+			{ "u", gitsigns.undo_stage_hunk, { desc = "Undo Last Stage" } },
+			{ "S", gitsigns.stage_buffer, { desc = "Stage Buffer" } },
+			{ "p", gitsigns.preview_hunk, { desc = "Preview Hunk" } },
+			{ "d", gitsigns.toggle_deleted, { nowait = true, desc = "Toggle Deleted" } },
+			{ "b", gitsigns.blame_line, { desc = "Blame" } },
+			{
+				"B",
+				function()
+					gitsigns.blame_line({ full = true })
+				end,
+				{ desc = "Blame Show Full" },
+			},
+			{ "/", gitsigns.show, { exit = true, desc = "Show Base File" } }, -- show the base of the file
+			{ "<Enter>", "<Cmd>Neogit<CR>", { exit = true, desc = "Neogit" } },
+			{ "q", nil, { exit = true, nowait = true, desc = "Exit" } },
+		},
+	}
 end
 function M.merge_conflict_menu()
-  local gitsigns = require "gitsigns"
+	local actions = require("diffview.actions")
 
-  local hint = [[
- _J_: Next hunk   _s_: Stage Hunk        _d_: Show Deleted   _b_: Blame Line
- _K_: Prev hunk   _u_: Undo Last Stage   _p_: Preview Hunk   _B_: Blame Show Full
- ^ ^              _S_: Stage Buffer      ^ ^                 _/_: Show Base File
- ^
- ^ ^              _<Enter>_: Neogit              _q_: Exit
+	local hint = [[
+  _o_: Ours               _t_: Theirs               _a_: All                _b_: Base
+  _O_: Ours Whole File    _T_: Theirs Whole file    _A_: All Whole File     _B_: Base Whole File
+
+  _S_: Stage File
+ ^ ^                      _m_: Finish Merge      _q_: Exit
 ]]
 
-  return {
-    name = "Git",
-    hint = hint,
-    config = {
-      color = "pink",
-      invoke_on_body = true,
-      hint = {
-        border = "rounded",
-        position = "bottom",
-      },
-      on_enter = function()
-        vim.cmd "mkview"
-        vim.cmd "silent! %foldopen!"
-        vim.bo.modifiable = false
-        gitsigns.toggle_signs(true)
-        gitsigns.toggle_linehl(true)
-      end,
-      on_exit = function()
-        vim.cmd("silent! DiffviewClose")
-        gitsigns.toggle_signs(false)
-        gitsigns.toggle_linehl(false)
-        gitsigns.toggle_deleted(false)
-      end,
-    },
-    body = "<space>gx",
-    heads = {
-      {
-        "J",
-        function()
-          if vim.wo.diff then
-            return "]c"
-          end
-          vim.schedule(function()
-            gitsigns.next_hunk()
-          end)
-          return "<Ignore>"
-        end,
-        { expr = true, desc = "Next Hunk" },
-      },
-      {
-        "K",
-        function()
-          if vim.wo.diff then
-            return "[c"
-          end
-          vim.schedule(function()
-            gitsigns.prev_hunk()
-          end)
-          return "<Ignore>"
-        end,
-        { expr = true, desc = "Prev Hunk" },
-      },
-      { "s", ":Gitsigns stage_hunk<CR>", { silent = true, desc = "Stage Hunk" } },
-      { "u", gitsigns.undo_stage_hunk,   { desc = "Undo Last Stage" } },
-      { "S", gitsigns.stage_buffer,      { desc = "Stage Buffer" } },
-      { "p", gitsigns.preview_hunk,      { desc = "Preview Hunk" } },
-      { "d", gitsigns.toggle_deleted,    { nowait = true, desc = "Toggle Deleted" } },
-      { "b", gitsigns.blame_line,        { desc = "Blame" } },
-      {
-        "B",
-        function()
-          gitsigns.blame_line { full = true }
-        end,
-        { desc = "Blame Show Full" },
-      },
-      { "/",       gitsigns.show,     { exit = true, desc = "Show Base File" } }, -- show the base of the file
-      { "<Enter>", "<Cmd>Neogit<CR>", { exit = true, desc = "Neogit" } },
-      { "q",       nil,               { exit = true, nowait = true, desc = "Exit" } },
-    },
-  }
+	return {
+		name = "Git Merge Conflict",
+		hint = hint,
+		config = {
+			color = "pink",
+			invoke_on_body = true,
+			hint = {
+				border = "rounded",
+				position = "bottom",
+			},
+		},
+		body = "<space>gx",
+		heads = {
+			{ "b", actions.conflict_choose("base"),{ desc = "Choose Base" } },
+			{ "B", actions.conflict_choose_all("base"),{ desc = "Choose Base All File" } },
+			{ "o", actions.conflict_choose("ours"),{ desc = "Choose Ours" } },
+			{ "O", actions.conflict_choose_all("ours"),{ desc = "Choose Ours All File" } },
+			{ "t", actions.conflict_choose("theirs"),{ desc = "Choose Theirs" } },
+			{ "T", actions.conflict_choose_all("theirs"),{ desc = "Choose Theirs All File" } },
+			{ "a", actions.conflict_choose("all"),{ desc = "Choose All" } },
+			{ "A", actions.conflict_choose_all("all"),{ desc = "Choose All for All File" } },
+			{ "m", "<cmd>Gin commit --<cr>",{ desc = "Finish Merge" } },
+			{ "S", "<cmd>Git add %<cr>",{ desc = "Stage file" } },
+			{ "q", nil, { exit = true, nowait = true, desc = "Exit" } },
+		},
+	}
 end
 
 function M.dap_menu()
-  local dap = require "dap"
-  local dapui = require "dapui"
-  local dap_widgets = require "dap.ui.widgets"
+	local dap = require("dap")
+	local dapui = require("dapui")
+	local dap_widgets = require("dap.ui.widgets")
 
-  local hint = [[
+	local hint = [[
  _t_: Toggle Breakpoint             _R_: Run to Cursor
  _s_: Start                         _E_: Evaluate Input
  _c_: Continue                      _C_: Conditional Breakpoint
@@ -183,19 +139,19 @@ function M.dap_menu()
  ^ ^               _q_: Quit
 ]]
 
-  return {
-    name = "Debug",
-    hint = hint,
-    config = {
-      color = "pink",
-      invoke_on_body = true,
-      hint = {
-        border = "rounded",
-        position = "middle-right",
-      },
-    },
-    mode = "n",
-    body = "<space>r.",
+	return {
+		name = "Debug",
+		hint = hint,
+		config = {
+			color = "pink",
+			invoke_on_body = true,
+			hint = {
+				border = "rounded",
+				position = "middle-right",
+			},
+		},
+		mode = "n",
+		body = "<space>r.",
     -- stylua: ignore
     heads = {
       { "C", function() dap.set_breakpoint(vim.fn.input "[Condition] > ") end, desc = "Conditional Breakpoint", },
@@ -230,25 +186,25 @@ function M.dap_menu()
         desc = "Exit"
       } },
     },
-  }
+	}
 end
 
 function M.lsp_menu()
-  local cmd = require("hydra.keymap-util").cmd
-  return {
-    name = "LSP Mode",
-    mode = { "n" },
-    config = {
-      color = "pink",
-      invoke_on_body = true,
-      hint = {
-        type = "window",
-        position = "bottom-right",
-        border = "rounded",
-        show_name = true,
-      },
-    },
-    hint = [[
+	local cmd = require("hydra.keymap-util").cmd
+	return {
+		name = "LSP Mode",
+		mode = { "n" },
+		config = {
+			color = "pink",
+			invoke_on_body = true,
+			hint = {
+				type = "window",
+				position = "bottom-right",
+				border = "rounded",
+				show_name = true,
+			},
+		},
+		hint = [[
     LSP
 ^
 Common Actions
@@ -268,38 +224,42 @@ Help
 ^
 _;_/_q_/_<Esc>_: Exit Hydra
 ]],
-    body = "<A-z>",
-    heads = {
-      { "s", cmd "TroubleToggle lsp_definitions",     { desc = "Jump to Definition", silent = true } },
-      { "h", cmd "Lspsaga hover_doc",                 { desc = "Show Hover Doc", silent = true } },
-      { "o", cmd "TroubleToggle lsp_implementations", { desc = "Show Implementations", silent = true } },
-      { "j", vim.lsp.buf.signature_help,              { desc = "Show Sig Help", silent = true } },
-      { "r", cmd "TroubleToggle lsp_references",      { desc = "Show References", silent = true } },
-      {
-        "f",
-        function()
-          vim.lsp.buf.format { async = true }
-        end,
-        { desc = "Format Buffer", silent = true },
-      },
-      { "a",     vim.lsp.buf.code_action,                   { desc = "Show Code Actions", silent = true } },
-      { "d",     cmd "TroubleToggle document_diagnostics",  { desc = "Show Diagnostics", silent = true } },
-      { "w",     cmd "TroubleToggle workspace_diagnostics", { desc = "Show Workspace Diagnostics", silent = true } },
-      { "D",     cmd "TroubleToggle lsp_definitions",       { desc = "Show Type Definition", silent = true } },
-      { "e",     vim.lsp.buf.declaration,                   { desc = "Show Declaration", silent = true } },
-      { ";",     nil,                                       { desc = "quit", exit = true, nowait = true } },
-      { "q",     nil,                                       { desc = "quit", exit = true, nowait = true } },
-      { "<Esc>", nil,                                       { desc = "quit", exit = true, nowait = true } },
-    },
-  }
+		body = "<A-z>",
+		heads = {
+			{ "s", cmd("TroubleToggle lsp_definitions"), { desc = "Jump to Definition", silent = true } },
+			{ "h", cmd("Lspsaga hover_doc"), { desc = "Show Hover Doc", silent = true } },
+			{ "o", cmd("TroubleToggle lsp_implementations"), { desc = "Show Implementations", silent = true } },
+			{ "j", vim.lsp.buf.signature_help, { desc = "Show Sig Help", silent = true } },
+			{ "r", cmd("TroubleToggle lsp_references"), { desc = "Show References", silent = true } },
+			{
+				"f",
+				function()
+					vim.lsp.buf.format({ async = true })
+				end,
+				{ desc = "Format Buffer", silent = true },
+			},
+			{ "a", vim.lsp.buf.code_action, { desc = "Show Code Actions", silent = true } },
+			{ "d", cmd("TroubleToggle document_diagnostics"), { desc = "Show Diagnostics", silent = true } },
+			{
+				"w",
+				cmd("TroubleToggle workspace_diagnostics"),
+				{ desc = "Show Workspace Diagnostics", silent = true },
+			},
+			{ "D", cmd("TroubleToggle lsp_definitions"), { desc = "Show Type Definition", silent = true } },
+			{ "e", vim.lsp.buf.declaration, { desc = "Show Declaration", silent = true } },
+			{ ";", nil, { desc = "quit", exit = true, nowait = true } },
+			{ "q", nil, { desc = "quit", exit = true, nowait = true } },
+			{ "<Esc>", nil, { desc = "quit", exit = true, nowait = true } },
+		},
+	}
 end
 
 function M.quick_menu()
-  local cmd = require("hydra.keymap-util").cmd
-  return {
-    name = "Quick Menu",
-    mode = { "n" },
-    hint = [[
+	local cmd = require("hydra.keymap-util").cmd
+	return {
+		name = "Quick Menu",
+		mode = { "n" },
+		hint = [[
         Quick Menu
 ^
 _f_: Show Terminal (float)
@@ -319,80 +279,105 @@ _m_: Show Man Pages
 ^
 ^ ^  _q_/_<Esc>_: Exit Hydra
     ]],
-    config = {
-      color = "teal",
-      invoke_on_body = true,
-      hint = {
-        type = "window",
-        position = "bottom",
-        border = "rounded",
-        show_name = true,
-      },
-    },
-    body = "<A-q>",
-    heads = {
-      { "t", cmd "Telescope help_tags", {
-        desc = "Open Help Tags",
-        silent = true
-      } },
-      { "k", ":lua require('telescope.builtin').keymaps()<CR>", {
-        desc = "Open Neovim Keymaps",
-        silent = true
-      } },
-      { "c", cmd "Telescope commands", {
-        desc =
-        "Open Available Telescope Commands",
-        silent = true
-      } },
-      { "m", cmd "Telescope man_pages", {
-        desc = "Opens Man Pages",
-        silent = true
-      } },
-      { "s", cmd "Telescope current_buffer_fuzzy_find skip_empty_lines=true",
-        {
-          desc =
-          "Fuzzy find in current buffer",
-          silent = true
-        } },
-      { "o", cmd "Telescope aerial", {
-        desc = "Opens Symbols Outline",
-        exit = true,
-        silent = true
-      } },
-      { "x", cmd "TroubleToggle quickfix", {
-        desc = "Opens Quickfix",
-        silent = true
-      } },
-      { "l", cmd "TroubleToggle loclist", {
-        desc = "Opens Location List",
-        silent = true
-      } },
+		config = {
+			color = "teal",
+			invoke_on_body = true,
+			hint = {
+				type = "window",
+				position = "bottom",
+				border = "rounded",
+				show_name = true,
+			},
+		},
+		body = "<A-q>",
+		heads = {
+			{ "t", cmd("Telescope help_tags"), {
+				desc = "Open Help Tags",
+				silent = true,
+			} },
+			{
+				"k",
+				":lua require('telescope.builtin').keymaps()<CR>",
+				{
+					desc = "Open Neovim Keymaps",
+					silent = true,
+				},
+			},
+			{
+				"c",
+				cmd("Telescope commands"),
+				{
+					desc = "Open Available Telescope Commands",
+					silent = true,
+				},
+			},
+			{ "m", cmd("Telescope man_pages"), {
+				desc = "Opens Man Pages",
+				silent = true,
+			} },
+			{
+				"s",
+				cmd("Telescope current_buffer_fuzzy_find skip_empty_lines=true"),
+				{
+					desc = "Fuzzy find in current buffer",
+					silent = true,
+				},
+			},
+			{
+				"o",
+				cmd("Telescope aerial"),
+				{
+					desc = "Opens Symbols Outline",
+					exit = true,
+					silent = true,
+				},
+			},
+			{ "x", cmd("TroubleToggle quickfix"), {
+				desc = "Opens Quickfix",
+				silent = true,
+			} },
+			{ "l", cmd("TroubleToggle loclist"), {
+				desc = "Opens Location List",
+				silent = true,
+			} },
 
-      { "f", cmd "ToggleTerm direction=float", {
-        desc = "Floating Terminal",
-        silent = true
-      } },
-      { "v", cmd "ToggleTerm direction=vertical", {
-        desc = "Vertical Terminal",
-        silent = true
-      } },
-      { "h", cmd "ToggleTerm direction=horizontal", {
-        desc = "Horizontal Terminal",
-        silent = true
-      } },
+			{
+				"f",
+				cmd("ToggleTerm direction=float"),
+				{
+					desc = "Floating Terminal",
+					silent = true,
+				},
+			},
+			{
+				"v",
+				cmd("ToggleTerm direction=vertical"),
+				{
+					desc = "Vertical Terminal",
+					silent = true,
+				},
+			},
+			{
+				"h",
+				cmd("ToggleTerm direction=horizontal"),
+				{
+					desc = "Horizontal Terminal",
+					silent = true,
+				},
+			},
 
-      { "q", nil, {
-        desc = "quit",
-        exit = true,
-        nowait = true
-      } },
-      { "<Esc>", nil, {
-        desc = "quit",
-        exit = true,
-        nowait = true
-      } },
-    },
-  }
+			{ "q", nil, {
+				desc = "quit",
+				exit = true,
+				nowait = true,
+			} },
+			{ "<Esc>", nil, {
+				desc = "quit",
+				exit = true,
+				nowait = true,
+			} },
+		},
+	}
 end
 
 return M
--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,45 +1,48 @@
 return {
-  {
-    "TimUntersberger/neogit",
-    cmd = "Neogit",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "sindrets/diffview.nvim",
-    },
-    opts=require("plugins-options.neogit"),
-  },
-  {
-    "lambdalisue/gin.vim",
-    cmd = { "Gin" },
-    dependencies = {
-      "vim-denops/denops.vim",
-    },
-  },
-  {
-    "kdheepak/lazygit.nvim",
-    cmd = {
-      "LazyGit",
-      "LazyGitCurrentFile",
-      "LazyGitFilterCurrentFile",
-      "LazyGitFilter",
-    },
-    config = function()
-      vim.g.lazygit_floating_window_scaling_factor = 1
-    end,
-  },
-  {
-    "lewis6991/gitsigns.nvim",
-    event = "BufReadPre",
-    opts = require("plugins-options.gitsigns")
-  },
-  {
-    "sindrets/diffview.nvim",
-    dependencies = "nvim-lua/plenary.nvim",
-    cmd = {
-      "DiffviewOpen",
-      "DiffviewFileHistory",
-    },
-    opts=require("plugins-options.diffview"),
-  }
-
+	{
+		"TimUntersberger/neogit",
+		cmd = "Neogit",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"sindrets/diffview.nvim",
+		},
+		opts = require("plugins-options.neogit"),
+	},
+	{
+		"lambdalisue/gin.vim",
+		cmd = { "Gin" },
+		dependencies = {
+			"vim-denops/denops.vim",
+		},
+	},
+	{
+		"kdheepak/lazygit.nvim",
+		cmd = {
+			"LazyGit",
+			"LazyGitCurrentFile",
+			"LazyGitFilterCurrentFile",
+			"LazyGitFilter",
+		},
+		config = function()
+			vim.g.lazygit_floating_window_scaling_factor = 1
+		end,
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+		event = "BufReadPre",
+		opts = require("plugins-options.gitsigns"),
+	},
+	{
+		"sindrets/diffview.nvim",
+		dependencies = "nvim-lua/plenary.nvim",
+		cmd = {
+			"DiffviewOpen",
+			"DiffviewFileHistory",
+		},
+		opts = require("plugins-options.diffview"),
+	},
+	{
+		"tpope/vim-fugitive",
+		cmd = "Git",
+	},
 }
