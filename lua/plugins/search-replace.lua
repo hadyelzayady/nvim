7d89d10a82e047563e6a12bd881b8f1e1f3e6ca3
hady.elzayady <hady.elzayady@pixelogicmedia.com>
2023-08-07 15:41:41 +0300

    jdlts

--- lua/plugins/search-replace.lua
+++ lua/plugins/search-replace.lua
@@ -1,13 +1,21 @@
 return {
-  {
-    -- better search
-    'kevinhwang91/nvim-hlslens',
-    config = function()
-      require('hlslens').setup()
-      require('config.keymaps').hlslens_setup()
-    end
-  },
-  {
-    "tpope/vim-abolish"
-  }
+	{
+		-- better search
+		"kevinhwang91/nvim-hlslens",
+		config = function()
+			require("hlslens").setup()
+			require("config.keymaps").hlslens_setup()
+		end,
+	},
+	{
+		"tpope/vim-abolish",
+	},
+	{
+		"eugen0329/vim-esearch",
+	},
+	{
+		"AckslD/muren.nvim",
+		opts = require("plugins-options.muren"),
+		cmd = { "MurenToggle", "MurenOpen", "MurenUnique", "MurenFresh" },
+	},
 }
