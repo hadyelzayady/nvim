d15604d3fa79313918835400e0a647db4b64adf8
hady.elzayady <hady.elzayady@pixelogicmedia.com>
2023-08-22 00:30:57 +0300

    eslint and prettier format

--- lua/plugins-options/formatter.lua
+++ lua/plugins-options/formatter.lua
@@ -1,46 +1,50 @@
 return function()
-  local util = require "formatter.util"
-  return {
-    -- Enable or disable logging
-    logging = true,
-    -- Set the log level
-    log_level = vim.log.levels.WARN,
-    -- All formatter configurations are opt-in
-    filetype = {
-      -- Formatter configurations for filetype "lua" go here
-      -- and will be executed in order
-      lua = {
-        require("formatter.filetypes.lua").stylua,
-      },
-      typescriptreact = {
-        require("formatter.filetypes.typescriptreact").prettierd,
-      },
-      javascriptreact = {
-        require("formatter.filetypes.typescriptreact").prettierd,
-      },
-      javascript = {
-        require("formatter.filetypes.typescriptreact").prettierd,
-      },
-      typescript = {
-        require("formatter.filetypes.typescriptreact").prettierd,
-      },
-      scss = {
-        require("formatter.filetypes.typescriptreact").prettierd,
-      },
+	local util = require("formatter.util")
+	return {
+		-- Enable or disable logging
+		logging = true,
+		-- Set the log level
+		log_level = vim.log.levels.WARN,
+		-- All formatter configurations are opt-in
+		filetype = {
+			-- Formatter configurations for filetype "lua" go here
+			-- and will be executed in order
+			lua = {
+				require("formatter.filetypes.lua").stylua,
+			},
+			typescriptreact = {
+				require("formatter.filetypes.typescriptreact").prettierd,
+				require("formatter.filetypes.typescriptreact").eslint_d,
+			},
+			javascriptreact = {
+				require("formatter.filetypes.typescriptreact").prettierd,
+				require("formatter.filetypes.typescriptreact").eslint_d,
+			},
+			javascript = {
+				require("formatter.filetypes.typescriptreact").prettierd,
+				require("formatter.filetypes.typescriptreact").eslint_d,
+			},
+			typescript = {
+				require("formatter.filetypes.typescriptreact").prettierd,
+				require("formatter.filetypes.typescriptreact").eslint_d,
+			},
+			scss = {
+				require("formatter.filetypes.typescriptreact").prettierd,
+			},
 
-      sh = {
-        require("formatter.filetypes.sh").shfmt,
-      },
-      python= {
-        require("formatter.filetypes.python").black,
-      },
+			sh = {
+				require("formatter.filetypes.sh").shfmt,
+			},
+			python = {
+				require("formatter.filetypes.python").black,
+			},
 
-      json= {
-        require("formatter.filetypes.json").prettierd,
-      },
-      ["*"] = {
-        require("formatter.filetypes.any").remove_trailing_whitespace
-      }
-    }
-  }
+			json = {
+				require("formatter.filetypes.json").prettierd,
+			},
+			["*"] = {
+				require("formatter.filetypes.any").remove_trailing_whitespace,
+			},
+		},
+	}
 end
--- lua/plugins/lsp/format.lua
+++ lua/plugins/lsp/format.lua
@@ -3,7 +3,7 @@ local M = {}
 function M.format()
   local filetype_table = require("formatter.config").values.filetype
   if filetype_table[vim.bo.filetype] then
-    return ":Format<cr>"
+    return ":FormatWriteLock<cr>"
   else
     vim.lsp.buf.format({async=true})
   end
