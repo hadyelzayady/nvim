c1f7a3cc535638f050b459a0f0687f5c98e9ffd3
hady.elzayady <hady.elzayady@pixelogicmedia.com>
2023-12-04 14:00:41 +0200

    matchu

--- lua/plugins/navigation.lua
+++ lua/plugins/navigation.lua
@@ -109,6 +109,17 @@ return {
 			vim.g.matchup_matchparen_deferred = 1 -- work async
 			-- vim.g.matchup_matchparen_offscreen = {} -- disable status bar icon
 			vim.g.matchup_matchparen_offscreen = { method = "popup" }
+			vim.g.matchup_matchpref = {
+				typescriptreact = {
+					tagnameonly = 1,
+				},
+				javacriptreact = {
+					tagnameonly = 1,
+				},
+				html = {
+					tagnameonly = 1,
+				},
+			}
 		end,
 	},
 }
