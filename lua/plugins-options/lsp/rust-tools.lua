3511d7a60328966d445115796ab18d6623e0b6ad
hadyelzayady <hadyelzayady1996@gmail.com>
2023-12-31 22:13:12 +0200

    rust fix all

--- lua/plugins-options/lsp/rust-tools.lua
+++ lua/plugins-options/lsp/rust-tools.lua
@@ -2,9 +2,27 @@ local M = {}
 
 function M.config(_)
 	require("rust-tools").setup({
+		tools = {
+			inlay_hints = {
+				auto = true,
+				show_parameter_hints = false,
+				parameter_hints_prefix = "",
+				other_hints_prefix = "",
+			},
+		},
 		server = {
 			-- on_attach = require("") on_attach,
 			capabilities = require("plugins-options.lsp.servers-configs").capabilities,
+			settings = {
+				-- to enable rust-analyzer settings visit:
+				-- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
+				["rust-analyzer"] = {
+					checkOnSave = true,
+					--      {
+					-- 	command = "clippy",
+					-- },
+				},
+			},
 		},
 	})
 end
