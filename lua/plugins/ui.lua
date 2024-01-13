d5e3637eedf69ecdda6f636ae71fd288c59aeef5
hadyelzayady <hadyelzayady1996@gmail.com>
2024-01-13 14:16:53 +0200

    reset ui

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -12,10 +12,10 @@ return {
 				end,
 			})
 			vim.cmd([[colorscheme tokyonight-night]])
-			vim.cmd("hi DiffAdd guifg=#26ac3c guibg=#303030 gui=reverse cterm=reverse")
-			vim.cmd("hi DiffChange guifg=#2b97b4 guibg=#303030 gui=reverse cterm=reverse")
-			vim.cmd("hi DiffDelete guifg=#c94300 guibg=#303030 gui=reverse cterm=reverse")
-			vim.cmd("hi DiffText guifg=#e99200 guibg=#303030 gui=reverse cterm=reverse")
+			-- vim.cmd("hi DiffAdd guifg=#26ac3c guibg=#303030 gui=reverse cterm=reverse")
+			-- vim.cmd("hi DiffChange guifg=#2b97b4 guibg=#303030 gui=reverse cterm=reverse")
+			-- vim.cmd("hi DiffDelete guifg=#c94300 guibg=#303030 gui=reverse cterm=reverse")
+			-- vim.cmd("hi DiffText guifg=#e99200 guibg=#303030 gui=reverse cterm=reverse")
 		end,
 	},
 	{
