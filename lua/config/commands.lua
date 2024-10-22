743d4b19725c6baae0258c6f0b4594b9f9886010
hady.elzayady <hady.elzayady@pixelogicmedia.com>
2024-10-22 20:31:53 +0300

    gitui

--- lua/config/commands.lua
+++ lua/config/commands.lua
@@ -11,5 +11,11 @@ function OpenLazyGitInKittyMaximizedWindow()
   -- Command to open a new window in Kitty with LazyGit
   os.execute("kitty @ launch --type=tab --cwd=current lazygit")
 end
+-- Function to open LazyGit in a new Kitty window and maximize it
+function OpenGitUIInKittyMaximizedWindow()
+  -- Command to open a new window in Kitty with LazyGit
+  os.execute("kitty @ launch --type=tab --cwd=current gitui")
+end
 
 vim.api.nvim_create_user_command('Lazygit', OpenLazyGitInKittyMaximizedWindow, {})
+vim.api.nvim_create_user_command('Gitui', OpenGitUIInKittyMaximizedWindow, {})
