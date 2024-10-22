997ff98f2462324370437d10098c8785f5819b43
hadyelzayady <hadyelzayady1996@gmail.com>
2024-10-22 09:55:54 +0300

    add lazygit

--- lua/config/commands.lua
+++ lua/config/commands.lua
@@ -1,7 +1,15 @@
 vim.api.nvim_create_user_command("DiffBuf", function(args)
-	if vim.opt.diff._value == false then
-		vim.cmd("windo diffthis")
-	else
-		vim.cmd("windo diffoff")
-	end
+  if vim.opt.diff._value == false then
+    vim.cmd("windo diffthis")
+  else
+    vim.cmd("windo diffoff")
+  end
 end, { range = true })
+
+-- Function to open LazyGit in a new Kitty window and maximize it
+function OpenLazyGitInKittyMaximizedWindow()
+  -- Command to open a new window in Kitty with LazyGit
+  os.execute("kitty @ launch --type=tab --cwd=current lazygit")
+end
+
+vim.api.nvim_create_user_command('Lazygit', OpenLazyGitInKittyMaximizedWindow, {})
