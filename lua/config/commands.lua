a7853315e0e57e80fa973daf6d40f4ff56021c9b
hady.elzayady <hady.elzayady@pixelogicmedia.com>
2024-10-28 10:50:47 +0300

    external git

--- lua/config/commands.lua
+++ lua/config/commands.lua
@@ -1,21 +1,19 @@
 vim.api.nvim_create_user_command("DiffBuf", function(args)
-  if vim.opt.diff._value == false then
-    vim.cmd("windo diffthis")
-  else
-    vim.cmd("windo diffoff")
-  end
+	if vim.opt.diff._value == false then
+		vim.cmd("windo diffthis")
+	else
+		vim.cmd("windo diffoff")
+	end
 end, { range = true })
 
 -- Function to open LazyGit in a new Kitty window and maximize it
 function OpenLazyGitInKittyMaximizedWindow()
-  -- Command to open a new window in Kitty with LazyGit
-  os.execute("kitty @ launch --type=tab --cwd=current lazygit")
+	vim.system({ "kitty", "@", "launch", "--type=tab", "--cwd=current", "lazygit" })
 end
 -- Function to open LazyGit in a new Kitty window and maximize it
 function OpenGitUIInKittyMaximizedWindow()
-  -- Command to open a new window in Kitty with LazyGit
-  os.execute("kitty @ launch --type=tab --cwd=current gitui")
+	vim.system({ "kitty", "@", "launch", "--type=tab", "--cwd=current", "gitui" })
 end
 
-vim.api.nvim_create_user_command('Lazygit', OpenLazyGitInKittyMaximizedWindow, {})
-vim.api.nvim_create_user_command('Gitui', OpenGitUIInKittyMaximizedWindow, {})
+vim.api.nvim_create_user_command("Lazygit", OpenLazyGitInKittyMaximizedWindow, {})
+vim.api.nvim_create_user_command("Gitui", OpenGitUIInKittyMaximizedWindow, {})
