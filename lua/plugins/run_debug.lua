0dfc1b20fd8f90bbb7fabb774c2d4cc450882ec7
hady.elzayady <hady.elzayady@pixelogicmedia.com>
2024-10-30 13:01:45 +0300

    add dap-ui

--- lua/plugins/run_debug.lua
+++ lua/plugins/run_debug.lua
@@ -1,6 +1,11 @@
 return {
-  {
-    "mfussenegger/nvim-dap",
-    config = require("plugins-options.dap").config
-  }
+	{
+		"mfussenegger/nvim-dap",
+		config = require("plugins-options.dap").config,
+	},
+	{
+		"rcarriga/nvim-dap-ui",
+		dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+		config = require("plugins-options.dap-ui").config,
+	},
 }
