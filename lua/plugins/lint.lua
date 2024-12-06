59857d8fdcf84fa3fc7720b9acb52e5dbcb880e2
hadyelzayady <hadyelzayady1996@gmail.com>
2024-12-06 20:35:20 +0200

    tsx snippet

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -4,7 +4,7 @@ return {
     event = "VeryLazy",
     config = function()
       require('lint').linters_by_ft = {
-        yaml = { 'cfn_lint' },
+        -- yaml = { 'cfn_lint' },
       }
       vim.api.nvim_create_autocmd({ "BufWritePost" }, {
         callback = function()
