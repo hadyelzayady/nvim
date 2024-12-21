c541279c643da194e45d3bc96ddb654e569247b1
hadyelzayady <hadyelzayady1996@gmail.com>
2024-12-21 19:19:29 +0200

    use blink

--- lua/plugins-options/lsp/lspconfig.lua
+++ lua/plugins-options/lsp/lspconfig.lua
@@ -1,40 +1,41 @@
 local M = {}
 
 function M.config()
-	local lspconfig = require("lspconfig")
+  local lspconfig = require("lspconfig")
 
-	local cmp_capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
-	local capabilities = vim.tbl_deep_extend(
-		"force",
-		cmp_capabilities,
-		-- returns configured operations if setup() was already called
-		-- or default operations if not
-		require("lsp-file-operations").default_capabilities()
-	)
+  -- local cmp_capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
+  local cmp_capabilities = require('blink.cmp').get_lsp_capabilities()
+  local capabilities = vim.tbl_deep_extend(
+    "force",
+    cmp_capabilities,
+    -- returns configured operations if setup() was already called
+    -- or default operations if not
+    require("lsp-file-operations").default_capabilities()
+  )
 
-	capabilities.textDocument.completion.completionItem.snippetSupport = true
-	-- Define a function to configure LSP servers
-	local function setup_lsp(server, config)
-		if config.capabilities == nil then
-			config.capabilities = capabilities
-		end
-		lspconfig[server].setup(config)
-	end
+  capabilities.textDocument.completion.completionItem.snippetSupport = true
+  -- Define a function to configure LSP servers
+  local function setup_lsp(server, config)
+    if config.capabilities == nil then
+      config.capabilities = capabilities
+    end
+    lspconfig[server].setup(config)
+  end
 
-	-- Configure individual servers
-	require("plugins-options.lsp.servers.vtsls")(setup_lsp)
-	require("plugins-options.lsp.servers.biome")(setup_lsp)
-	require("plugins-options.lsp.servers.cssmodules")(setup_lsp)
-	require("plugins-options.lsp.servers.cssls")(setup_lsp)
-	require("plugins-options.lsp.servers.lua")(setup_lsp)
-	require("plugins-options.lsp.servers.eslint")(setup_lsp)
-	require("plugins-options.lsp.servers.java")(setup_lsp)
-	require("plugins-options.lsp.servers.json")(setup_lsp)
-	require("plugins-options.lsp.servers.yaml")(setup_lsp)
-	require("plugins-options.lsp.servers.prisma")(setup_lsp)
-	require("plugins-options.lsp.servers.nginx")(setup_lsp)
-	require("plugins-options.lsp.servers.tailwind")(setup_lsp)
-	require("config.keymaps").lsp()
+  -- Configure individual servers
+  require("plugins-options.lsp.servers.vtsls")(setup_lsp)
+  require("plugins-options.lsp.servers.biome")(setup_lsp)
+  require("plugins-options.lsp.servers.cssmodules")(setup_lsp)
+  require("plugins-options.lsp.servers.cssls")(setup_lsp)
+  require("plugins-options.lsp.servers.lua")(setup_lsp)
+  require("plugins-options.lsp.servers.eslint")(setup_lsp)
+  require("plugins-options.lsp.servers.java")(setup_lsp)
+  require("plugins-options.lsp.servers.json")(setup_lsp)
+  require("plugins-options.lsp.servers.yaml")(setup_lsp)
+  require("plugins-options.lsp.servers.prisma")(setup_lsp)
+  require("plugins-options.lsp.servers.nginx")(setup_lsp)
+  require("plugins-options.lsp.servers.tailwind")(setup_lsp)
+  require("config.keymaps").lsp()
 end
 
 return M
--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,52 +1,72 @@
 return {
-	{
-		"hrsh7th/nvim-cmp",
-		config = require("plugins-options.completion.cmp").config,
-		event = "InsertEnter",
-		dependencies = {
-			"hrsh7th/cmp-nvim-lsp",
-			"hrsh7th/cmp-buffer",
-			"hrsh7th/cmp-path",
-			"hrsh7th/cmp-calc",
-			"saadparwaiz1/cmp_luasnip",
-			"rcarriga/cmp-dap",
-		},
-	},
-	{
-		"L3MON4D3/LuaSnip",
-		version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-		build = "make install_jsregexp",
-		dependencies = {
-			"rafamadriz/friendly-snippets",
-			config = function()
-				require("luasnip.loaders.from_vscode").lazy_load()
-			end,
-		},
-		keys = {
-			{
-				"<tab>",
-				function()
-					return require("luasnip").jumpable(1) and "<Plug>luasnip-jump-next" or "<tab>"
-				end,
-				expr = true,
-				silent = true,
-				mode = "i",
-			},
-			{
-				"<tab>",
-				function()
-					require("luasnip").jump(1)
-				end,
-				mode = "s",
-			},
-			{
-				"<s-tab>",
-				function()
-					require("luasnip").jump(-1)
-				end,
-				mode = { "i", "s" },
-			},
-		},
-		config = require("plugins-options.completion.luasnip").config,
-	},
+  {
+    'saghen/blink.cmp',
+    dependencies = 'rafamadriz/friendly-snippets',
+
+    version = '*',
+    opts = {
+      keymap = { preset = 'default' },
+
+      appearance = {
+        use_nvim_cmp_as_default = true,
+        nerd_font_variant = 'mono'
+      },
+      signature = { enabled = true },
+      sources = {
+        default = { 'lsp', 'path', 'snippets', 'buffer' },
+      },
+    },
+    opts_extend = { "sources.default" }
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    config = require("plugins-options.completion.cmp").config,
+    event = "InsertEnter",
+    enabled = false,
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-calc",
+      "saadparwaiz1/cmp_luasnip",
+      "rcarriga/cmp-dap",
+    },
+  },
+  {
+    "L3MON4D3/LuaSnip",
+    version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+    build = "make install_jsregexp",
+    dependencies = {
+      "rafamadriz/friendly-snippets",
+      config = function()
+        require("luasnip.loaders.from_vscode").lazy_load()
+      end,
+    },
+    keys = {
+      {
+        "<tab>",
+        function()
+          return require("luasnip").jumpable(1) and "<Plug>luasnip-jump-next" or "<tab>"
+        end,
+        expr = true,
+        silent = true,
+        mode = "i",
+      },
+      {
+        "<tab>",
+        function()
+          require("luasnip").jump(1)
+        end,
+        mode = "s",
+      },
+      {
+        "<s-tab>",
+        function()
+          require("luasnip").jump(-1)
+        end,
+        mode = { "i", "s" },
+      },
+    },
+    config = require("plugins-options.completion.luasnip").config,
+  },
 }
