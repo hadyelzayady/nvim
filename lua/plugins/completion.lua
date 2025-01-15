93bc35325b7b6a799e8de77f95b624c6307f5dfd
hadyelzayady <hadyelzayady1996@gmail.com>
2025-01-15 08:47:59 +0200

    ui and formatter

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,13 +1,13 @@
 local no_pre_select_filetypes = { "DressingInput", "minifiles" }
 return {
 	{
-		'saghen/blink.cmp',
+		"saghen/blink.cmp",
 		event = "InsertEnter",
-		dependencies = { 'rafamadriz/friendly-snippets', "mikavilpas/blink-ripgrep.nvim" },
-		version = '*',
+		dependencies = { "rafamadriz/friendly-snippets", "mikavilpas/blink-ripgrep.nvim" },
+		version = "*",
 		opts = {
 			keymap = {
-				preset = 'enter',
+				preset = "enter",
 				["<c-g>"] = {
 					function()
 						-- invoke manually, requires blink >v0.8.0
@@ -21,21 +21,34 @@ return {
 						auto_insert = false,
 						preselect = function(ctx)
 							local filetype = vim.bo[ctx.bufnr].filetype
-							return ctx.mode ~= 'cmdline' and not require('blink.cmp').snippet_active({ direction = 1 }) and
-								not vim.tbl_contains(no_pre_select_filetypes, filetype)
+							return ctx.mode ~= "cmdline"
+								and not require("blink.cmp").snippet_active({ direction = 1 })
+								and not vim.tbl_contains(no_pre_select_filetypes, filetype)
 						end,
-					}
+					},
 				},
 				menu = {
 					draw = {
-						treesitter = { 'lsp' },
-						columns = { { "label", "label_description", gap = 1 }, { "kind_icon", "kind", "source_name" } }
-					}
-				}
+						treesitter = { "lsp" },
+						columns = { { "label", "label_description", gap = 1 }, { "kind_icon", "kind", "source_name" } },
+					},
+				},
+				accept = {
+					-- auto_brackets = {
+					-- 	kind_resolution = {
+					-- 		enabled = true,
+					-- 		blocked_filetypes = { "typescriptreact", "javascriptreact", "vue" },
+					-- 	},
+					-- 	semantic_token_resolution = {
+					-- 		enabled = true,
+					-- 		blocked_filetypes = { "typescript" },
+					-- 	},
+					-- },
+				},
 			},
 			signature = { enabled = true },
 			sources = {
-				default = { 'lsp', 'path', 'snippets', 'buffer' },
+				default = { "lsp", "path", "snippets", "buffer" },
 				cmdline = {},
 				providers = {
 					ripgrep = {
@@ -46,9 +59,9 @@ return {
 			},
 
 			appearance = {
-				kind_icons = require("utils.ui-components").icons.kinds
-			}
+				kind_icons = require("utils.ui-components").icons.kinds,
+			},
 		},
-		opts_extend = { "sources.default" }
+		opts_extend = { "sources.default" },
 	},
 }
