7de6850af524568d725a992c474895331a20c143
hadyelzayady <hadyelzayady1996@gmail.com>
2025-03-28 23:04:06 +0200

    etra

--- lua/plugins-new/extra.lua
+++ lua/plugins-new/extra.lua
@@ -1,41 +1,42 @@
 return {
 
-    {
-        "folke/which-key.nvim",
-        event = "VeryLazy",
-        dependencies = {
-            { "echasnovski/mini.icons" },
-        },
-        opts = require("plugins-options.which-key").opts,
-        keys = {
-            {
-                "<leader>?",
-                function()
-                    require("which-key").show({ global = false })
-                end,
-                desc = "Buffer Local Keymaps (which-key)",
-            },
-        },
-    },
-    {
-        "folke/snacks.nvim",
-        lazy = false,
-        ---@type snacks.Config
-        opts = {
-            input = {
-                position = "right",
-                -- your input configuration comes here
-                -- or leave it empty to use the default settings
-                -- refer to the configuration section below
-            },
-            styles = {
-                input = {
-                    relative = "cursor",
-                    row = -3,
-                    col = 0
-
-                }
-            }
-        }
-    }
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		dependencies = {
+			{ "echasnovski/mini.icons" },
+		},
+		opts = require("plugins-options.which-key").opts,
+		keys = {
+			{
+				"<leader>?",
+				function()
+					require("which-key").show({ global = false })
+				end,
+				desc = "Buffer Local Keymaps (which-key)",
+			},
+		},
+	},
+	{
+		"folke/snacks.nvim",
+		lazy = false,
+		priority = 1000,
+		---@type snacks.Config
+		opts = {
+			bigfile = { enabled = true },
+			input = {
+				position = "right",
+				-- your input configuration comes here
+				-- or leave it empty to use the default settings
+				-- refer to the configuration section below
+			},
+			styles = {
+				input = {
+					relative = "cursor",
+					row = -3,
+					col = 0,
+				},
+			},
+		},
+	},
 }
