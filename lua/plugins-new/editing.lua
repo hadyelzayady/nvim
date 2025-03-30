c20b18f945f7d673608911dfbda52850b1067cae
hadyelzayady <hadyelzayady1996@gmail.com>
2025-03-30 17:55:53 +0200

    tools

--- lua/plugins-new/editing.lua
+++ lua/plugins-new/editing.lua
@@ -23,6 +23,21 @@ return {
 		keys = { { "s", mode = { "v", "n" } } },
 	},
 	{
+		"echasnovski/mini.ai",
+		version = false,
+		event = "VeryLazy",
+		dependencies = {
+			{
+				"nvim-treesitter/nvim-treesitter-textobjects",
+				init = function()
+					-- no need to load the plugin, since we only need its queries
+					require("lazy.core.loader").disable_rtp_plugin("nvim-treesitter-textobjects")
+				end,
+			},
+		},
+		config = require("plugins-options.mini-ai").config,
+	},
+	{
 		"mbbill/undotree",
 		cmd = "UndotreeToggle",
 	},
