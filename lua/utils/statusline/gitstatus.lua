8e1e1df75173087cb59dd6bd8a8d52a85d4e2200
hadyelzayady <hadyelzayady1996@gmail.com>
2025-04-09 10:56:22 +0200

    fix git head

--- lua/utils/statusline/gitstatus.lua
+++ lua/utils/statusline/gitstatus.lua
@@ -10,25 +10,20 @@ local function update_git_ahead_behind()
 	last_update = now
 
 	-- Run git status asynchronously
-	vim.system(
-		{ "git", "rev-list", "--left-right", "--count", "HEAD...@{upstream}", "2>/dev/null" },
-		{ text = true },
-		function(res)
-			if res.code == 0 and res.stdout then
-				local status = res.stdout:match("(%d+)%s+(%d+)")
-				local ahead, behind = status:match("(%d+)%s+(%d+)")
-				ahead, behind = tonumber(ahead), tonumber(behind)
-				local result = ""
-				if ahead and ahead > 0 then
-					result = result .. " ⇡" .. ahead .. " "
-				end
-				if behind and behind > 0 then
-					result = result .. " ⇣" .. behind
-				end
-				git_ahead_behind = result
+	vim.system({ "git", "rev-list", "--left-right", "--count", "HEAD...@{upstream}" }, { text = true }, function(res)
+		if res.code == 0 and res.stdout then
+			local ahead, behind = res.stdout:match("(%d+)\t+(%d+)")
+			ahead, behind = tonumber(ahead), tonumber(behind)
+			local result = ""
+			if ahead and ahead > 0 then
+				result = result .. " ⇡" .. ahead .. " "
 			end
+			if behind and behind > 0 then
+				result = result .. " ⇣" .. behind
+			end
+			git_ahead_behind = result
 		end
-	)
+	end)
 end
 
 -- Run update on events instead of every redraw
