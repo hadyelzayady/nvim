528871a7a9455b88cec50cadca0cdd8df51d1746
hady.elzayady <hady.elzayady@pixelogicmedia.com>
2025-07-10 16:40:39 +0300

    git commit

--- after/ftplugin/gitcommit.lua
+++ after/ftplugin/gitcommit.lua
@@ -1,8 +1,7 @@
--- vim.keymap.set("n", "<leader>c", function()
--- 	require("codecompanion").prompt("commit")
--- end, { noremap = true, silent = true, buffer = true })
---
--- vim.keymap.set("n", "<leader>C", function()
--- 	require("codecompanion").prompt("commitChat")
--- end, { noremap = true, silent = true, buffer = true })
---
+vim.keymap.set("n", "<leader>c", function()
+	require("codecompanion").prompt("commit")
+end, { noremap = true, silent = true, buffer = true })
+
+vim.keymap.set("n", "<leader>C", function()
+	require("codecompanion").prompt("commitChat")
+end, { noremap = true, silent = true, buffer = true })
