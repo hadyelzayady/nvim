923833f16c742c9bdf4daed27b0c69d206465dc1
hady.elzayady <hady.elzayady@pixelogicmedia.com>
2025-07-10 19:41:56 +0300

    jdtls

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,27 +3,15 @@ return {
 		"neovim/nvim-lspconfig",
 		lazy = true,
 		event = { "BufReadPre", "BufNewFile" },
-		-- dependencies = {
-		-- 	-- { "ms-jpq/coq_nvim", branch = "coq" },
-		-- 	-- { "ms-jpq/coq.artifacts", branch = "artifacts" },
-		-- 	-- { "ms-jpq/coq.thirdparty", branch = "3p" },
-		-- },
-		-- init = function()
-		-- 	vim.g.coq_settings = {
-		-- 		auto_start = true, -- or "true" for always on
-		-- 		display = {
-		-- 			statusline = {
-		-- 				helo = false,
-		-- 			},
-		-- 		},
-		-- 	}
-		-- 	require("plugins.config.coq_3p").config()
-		-- end,
 	},
 	{
 		"stevearc/conform.nvim",
 		lazy = true,
-		---@diagnostic disable-next-line: different-requires
 		opts = require("plugins.config.conform").opts,
+	},
+	{
+		"nvim-java/nvim-java",
+		dependencies = { "neovim/nvim-lspconfig" },
+		opts = {},
 	},
 }
