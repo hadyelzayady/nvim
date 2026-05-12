611859d9e0eaa0fd927fe745c1c6dca6e4320d73
hady.elzayady <hady.elzayady@pixelogicmedia.com>
2026-05-12 15:07:55 +0300

    fix octo

--- after/plugin/octo.lua
+++ after/plugin/octo.lua
@@ -1,11 +1,19 @@
-Config.new_autocmd("CmdUndefined", "Octo", function()
-	vim.defer_fn(function()
-		local ok, octo = pcall(require, "octo")
-		if ok then
-			octo.setup({
-				picker = "fzf-lua",
-				use_local_fs = true,
-			})
-		end
-	end, 0)
-end, "Lazy load octo on Octo command")
+-- Config.new_autocmd("CmdUndefined", "Octo", function()
+-- 	vim.defer_fn(function()
+-- 		local ok, octo = pcall(require, "octo")
+-- 		if ok then
+-- 			octo.setup({
+-- 				picker = "fzf-lua",
+-- 				use_local_fs = true,
+-- 			})
+-- 		end
+-- 	end, 0)
+-- end, "Lazy load octo on Octo command")
+
+local ok, octo = pcall(require, "octo")
+if ok then
+	octo.setup({
+		picker = "fzf-lua",
+		use_local_fs = true,
+	})
+end
