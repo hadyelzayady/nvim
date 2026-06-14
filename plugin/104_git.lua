eae59a6e94e3b5b1458a1d173fe0e8df9974c1cb
hady.elzayady <hady.elzayady@pixelogicmedia.com>
2026-06-14 11:55:02 +0300

    update lock

--- plugin/104_git.lua
+++ plugin/104_git.lua
@@ -1,7 +1,7 @@
 vim.pack.add({
 	{ src = "https://github.com/nvim-lua/plenary.nvim" },
 	{ src = "https://github.com/tpope/vim-fugitive" },
-	{ src = "https://github.com/dlyongemallo/diffview.nvim" },
+	{ src = "https://github.com/dlyongemallo/diffview-plus.nvim" },
 	{ src = "https://github.com/pwntester/octo.nvim" },
 	{ src = "https://github.com/emmanueltouzery/agitator.nvim" },
 	{ src = "https://github.com/esmuellert/codediff.nvim" },
